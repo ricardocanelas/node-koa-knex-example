ba6e94ea09e6892ad6a840057f7d4cd55c3c73c9
Ricardo Canelas <ricardocanelas@gmail.com>
2019-12-09 18:27:16 -0300

    chore(babel): update

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  presets: ['@babel/preset-env'],
-}
+// https://github.com/facebook/jest/issues/7579#issuecomment-457069424
+module.exports =
+  process.env.NODE_ENV === 'test'
+    ? {
+        presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+      }
+    : {
+        presets: ['@babel/preset-env'],
+      }
